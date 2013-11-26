189029fff6cbdce007492b0f94d14187014cb6ee
Ole Michelsen <ole@michelsen.dk>
2013-11-26 17:59:07 +0100

    Changed from widget pattern to normal plugin (way less code).

--- jquery.formnavigation.js
+++ jquery.formnavigation.js
@@ -3,21 +3,10 @@
 * Copyright 2013 Ole Bjørn Michelsen <http://ole.michelsen.dk/>
 * MIT license
 */
-;(function ($, undefined) {
-    var pluginName = 'formNavigation';
-    var defaults = {};
-
-    function Plugin(element, options) {
-        this.element = element;
-        this.options = $.extend( {}, defaults, options);
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
-
-    Plugin.prototype.init = function () {
-        $(this.element)
-            .find('input').on('keyup', function(e) {
+(function ($) {
+    $.fn.formNavigation = function () {
+        $(this).each(function () {
+            $(this).find('input').on('keyup', function(e) {
                 switch (e.which) {
                     case 39:
                         $(this).closest('td').next().find('input').focus(); break;
@@ -29,19 +18,6 @@
                         $(this).closest('tr').prev().children().eq($(this).closest('td').index()).find('input').focus(); break;
                 }
             });
-    };
-
-    Plugin.prototype.destroy = function () {
-        $(this.element).off('.' + this._name); // remove plugin event handlers
-        $.removeData(this.element, 'plugin_' + this._name); // remove plugin data
-    };
-
-    $.fn[pluginName] = function (options) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
-                new Plugin(this, options));
-            }
         });
     };
 })(jQuery);
